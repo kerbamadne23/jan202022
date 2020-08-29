250b2f78a49028da45700b84a076e32ca0fdbf99
kerba <kerba.madne14@gmail.com>
2020-08-29 19:04:39 +0530

    eclipse-workspace/BasicJavaProgram/.classpath

---
+++ eclipse-workspace/BasicJavaProgram/src/com/method/MethodExample.java
@@ -0,0 +1,31 @@
+package com.method;
+
+public class MethodExample {
+	public void addtion() {
+		int num1 = 10;
+		int num2 = 20;
+		int sum = num1 + num2;
+		System.out.println(sum);
+	}
+
+	public void subtration() {
+		int num1 = 10;
+		int num2 = 20;
+		int sub = num1 - num2;
+		System.out.println(sub);
+	}
+
+	public void multi() {
+		int num1 = 10;
+		int num2 = 20;
+		int Mult = num1 * num2;
+		System.out.println(Mult);
+	}
+
+	public void division() {
+		int num1 = 10;
+		int num2 = 20;
+		int div = num2 / num1;
+		System.out.println(div);
+	}
+}
---
+++ eclipse-workspace/BasicJavaProgram/src/com/method/MethodTest.java
@@ -0,0 +1,13 @@
+package com.method;
+
+public class MethodTest {
+
+	public static void main(String[] args) {
+		MethodExample me = new MethodExample();
+		me.addtion();
+		me.subtration();
+		me.multi();
+		me.division();
+	}
+
+}
